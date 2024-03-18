bdbcd94987e94c2e283bf292114a8ec8a3467052
akhiltadikamalla9 <147491162+akhiltadikamalla9@users.noreply.github.com>
2024-03-19 03:47:14 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -3,5 +3,5 @@ import sys
 # Get the argument passed from Jenkins
 input_param = sys.argv[1]
 print(f"Received input: {input_param}")
-input_param2 = sys.argv[2]
+input_param2 = sys.argv[1]
 print(f"Received input: {input_param2}")
