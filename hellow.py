23549387a652170c1d3e95cffaa522455d902fd4
akhiltadikamalla9 <147491162+akhiltadikamalla9@users.noreply.github.com>
2024-03-25 01:27:50 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -3,3 +3,6 @@ import sys
 # Get the argument passed from Jenkins
 param1_list = sys.argv[1].split(',')
 param2_list = sys.argv[2].split(',')
+# Print the ArrayLists for verification
+print("Param1 List:", param1_list)
+print("Param2 List:", param2_list)
